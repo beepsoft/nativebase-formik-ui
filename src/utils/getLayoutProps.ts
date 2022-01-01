73652a028dfdae1edfd0b2cbbd71a728d2f30e2b
Balazs E. Pataki <pataki@gmail.com>
2022-01-01 22:57:52 +0100

    Merge branch 'master' of github.com:nativebase/nativebase-formik-ui

--- src/utils/getLayoutProps.ts
+++ src/utils/getLayoutProps.ts
@@ -34,6 +34,10 @@ export const getLayoutProps = (props: any) => {
     'bottom',
     'top',
     'isRequired',
+    'w',
+    'width',
+    'h',
+    'height',
   ];
   return extractInObject(props, layoutProps);
 };
